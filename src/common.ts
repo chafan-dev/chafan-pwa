747940ab80eb7b23b040b5a6dbe07744b1544ef5
Zhen Zhang <git@zhen-zhang.com>
2021-03-05 03:51:58 -0800

    Fix SimpleEditor/SimpleViewer

--- src/common.ts
+++ src/common.ts
@@ -1,2 +1,2 @@
 export const URLRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?\b([-a-zA-Z0-9()@:,%_\+.~#?&//=]*)/gi;
-export const vditorCDN = 'https://cdn.jsdelivr.net/npm/@chafan/vditor@3.8.1-chafan.4';
+export const vditorCDN = 'https://cdn.jsdelivr.net/npm/@chafan/vditor@3.8.2-chafan.2';
