79436f36126f4dd5f176eebd4fb307aeee31d16d
Zhen Zhang <git@zhen-zhang.com>
2021-03-08 23:37:08 -0800

    Fix minor issue

--- src/views/main/Article.vue
+++ src/views/main/Article.vue
@@ -11,7 +11,6 @@
                         </span>
                         <v-spacer />
                         <span class="mr-3">
-                            <!-- {{$t('发表于专栏')}} -->
                             <a class="text-decoration-none" target="_blank"
                                :href="`/article-columns/${article.article_column.uuid}`" >
                                 {{ article.article_column.name }}
@@ -43,35 +42,34 @@
                                             </v-card-title>
                                         <v-card-actions>
                                             <v-spacer />
-                                            <v-btn @click="showCancelUpvoteDialog = false">{{$t('No')}}</v-btn>
-                                            <v-btn color="warning" @click="cancelUpvote" :disabled='cancelUpvoteIntermediate'>{{$t('Yes')}}</v-btn>
+                                            <v-btn small depressed class="mx-1" @click="showCancelUpvoteDialog = false">{{$t('No')}}</v-btn>
+                                            <v-btn small depressed color="warning" @click="cancelUpvote" :disabled='cancelUpvoteIntermediate'>{{$t('Yes')}}</v-btn>
                                         </v-card-actions>
                                         </v-card>
                                     </v-dialog>
 
-                                    <v-btn @click="showCancelUpvoteDialog = true"
-                                        :small="!$vuetify.breakpoint.mdAndUp" class="slim-btn ma-1" color="primary lighten-2" v-if="upvotes && upvotes.upvoted">
+                                    <v-btn small depressed class="mx-1" @click="showCancelUpvoteDialog = true"
+                                           color="primary lighten-2" v-if="upvotes && upvotes.upvoted">
                                         {{$t('已赞')}} ({{ upvotes.count }})
                                     </v-btn>
-                                    <v-btn :small="!$vuetify.breakpoint.mdAndUp" class="slim-btn ma-1" color="primary" @click="upvote"
-                                            v-else :disabled="currentUserIsAuthor || upvoteIntermediate">
+                                    <v-btn small depressed class="mx-1" color="primary" @click="upvote"
+                                           v-else :disabled="currentUserIsAuthor || upvoteIntermediate">
                                         {{$t('赞')}} ({{ upvotes.count }})
                                     </v-btn>
                                 </template>
 
-                                <v-btn :small="!$vuetify.breakpoint.mdAndUp" class="slim-btn ma-1"
+                                <v-btn small depressed class="mx-1"
                                        @click="toggleShowComments">
                                     {{article.comments.length == 0 ? $t('评论') : $t('查看n条评论', {amount: article.comments.length})}}
                                 </v-btn>
 
-                                <v-btn :small="!$vuetify.breakpoint.mdAndUp" class="slim-btn ma-1"
+                                <v-btn small depressed class="mx-1"
                                        :href="`/article-focus?articleColumnId=${this.article.article_column.uuid}&articleId=${this.article.uuid}`"
                                        v-show="currentUserIsAuthor">{{$t(editButtonText)}}</v-btn>
 
                                 <v-menu offset-y v-if="currentUserIsAuthor">
                                     <template v-slot:activator="{ on, attrs }">
-                                        <v-btn v-bind="attrs" v-on="on" class="slim-btn ma-1"
-                                            :small="!$vuetify.breakpoint.mdAndUp" >{{$t('设置')}}</v-btn>
+                                        <v-btn v-bind="attrs" v-on="on" small depressed class="mx-1">{{$t('设置')}}</v-btn>
                                     </template>
                                     <v-list>
                                         <v-list-item @click="confirmDeleteDialog = true">
@@ -90,8 +88,8 @@
                                         </v-card-title>
                                     <v-card-actions>
                                         <v-spacer />
-                                        <v-btn @click="confirmDeleteDialog = false">{{$t('No')}}</v-btn>
-                                        <v-btn color="warning" @click="deleteArticle" :disabled='deleteArticleIntermediate'>{{$t('Yes')}}</v-btn>
+                                        <v-btn small depressed class="mx-1" @click="confirmDeleteDialog = false">{{$t('No')}}</v-btn>
+                                        <v-btn small depressed class="mx-1" color="warning" @click="deleteArticle" :disabled='deleteArticleIntermediate'>{{$t('Yes')}}</v-btn>
                                     </v-card-actions>
                                     </v-card>
                                 </v-dialog>
@@ -116,8 +114,6 @@
                         <div class="d-flex justify-end mt-1">
                             <ReactionBlock objectType="article" class="ml-1" :objectId="article.uuid"/>
                         </div>
-
-
 
                         <!-- Comments -->
                         <v-expand-transition>
