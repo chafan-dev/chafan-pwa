ba7e3ab95d4be1c486870c5a57a598590d498386
agdholo <agdholo@gmail.com>
2021-03-14 18:38:20 +0800

    refactor(UserWelcome): separate the independent welcome components

--- src/components/ExploreSitesGrid.vue
+++ src/components/ExploreSitesGrid.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row v-if="!loading">
-    <v-col v-for="(blocks, idx) in allBlocks" :key="idx">
-      <v-card v-for="(block, idx) in blocks" :key="idx" class="mb-4 pa-2">
-        <h3>{{ block.topicName }}</h3>
-        <div>
-          <SiteBtn :showHotness="true" :site="site" :key="site.uuid" v-for="site in block.sites" />
+    <v-col v-for="(blocks, idx) in allBlocks" :key="idx" class="pb-2">
+      <base-card v-for="(block, idx) in blocks" :key="idx">
+        <h3 class="body-1">{{ block.topicName }}</h3>
+        <div class="mt-2">
+          <SiteBtn v-for="site in block.sites" :key="site.uuid" :showHotness="true" :site="site" />
         </div>
-      </v-card>
+      </base-card>
     </v-col>
   </v-row>
 </template>
@@ -17,6 +17,7 @@ import { ISite, ISiteMap } from '@/interfaces';
 import SiteBtn from '@/components/SiteBtn.vue';
 import { Component, Vue } from 'vue-property-decorator';
 import { dispatchCaptureApiError } from '@/store/main/actions';
+import BaseCard from '@/components/base/BaseCard.vue';
 
 function uniqBy(a, key) {
   const seen = new Set();
@@ -27,18 +28,18 @@ function uniqBy(a, key) {
 }
 
 @Component({
-  components: { SiteBtn },
+  components: { BaseCard, SiteBtn },
 })
 export default class ExploreSitesGrid extends Vue {
-  get allBlocks() {
-    return [this.blocks1, this.blocks2];
-  }
   private blocks1: Array<{ topicName: string; sites: ISite[] }> = [];
   private blocks1Weight = 0;
   private blocks2: Array<{ topicName: string; sites: ISite[] }> = [];
   private blocks2Weight = 0;
-
   private loading = true;
+
+  get allBlocks() {
+    return [this.blocks1, this.blocks2];
+  }
 
   private sitesFromSiteMap(siteMap: ISiteMap): ISite[] {
     return siteMap.sites.concat(
---
+++ src/components/user/UserWelcome.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-sheet>
+    <base-card :embedded="true" :no-gutter="true" outlined>
+      <v-alert type="info">
+        <template v-slot:prepend>
+          <v-btn icon>
+            <InfoIcon />
+          </v-btn>
+        </template>
+
+        {{ $t('刚刚加入？') }}
+
+        <template v-slot:close>
+          <v-btn icon @click="$emit('on-close-explore-sites')">
+            <close-icon />
+          </v-btn>
+        </template>
+      </v-alert>
+
+      <div>
+        <v-stepper class="elevation-0" value="1" vertical>
+          <v-stepper-step step="1">
+            <div class="d-flex">
+              {{ $t('完善') }}
+
+              <router-link class="text-decoration-none" to="/profile/edit">
+                {{ $t('个人页面') }}
+              </router-link>
+
+              {{ $t('来告诉朋友们你是谁') }}
+            </div>
+          </v-stepper-step>
+
+          <v-stepper-content step="1" />
+
+          <v-stepper-step step="2">
+            {{ $t('探索 Chafan 上的感兴趣的圈子和用户来生成自己的信息流：') }}
+          </v-stepper-step>
+
+          <v-stepper-content step="2" />
+        </v-stepper>
+      </div>
+    </base-card>
+
+    <explore-sites-grid class="mt-2" />
+  </v-sheet>
+</template>
+<script lang="ts">
+import { Component, Emit, Vue } from 'vue-property-decorator';
+import ExploreSitesGrid from '@/components/ExploreSitesGrid.vue';
+import BaseCard from '@/components/base/BaseCard.vue';
+import CloseIcon from '@/components/icons/CloseIcon.vue';
+import InfoIcon from '@/components/icons/InfoIcon.vue';
+
+@Component({
+  components: { InfoIcon, CloseIcon, BaseCard, ExploreSitesGrid },
+})
+export default class UserWelcome extends Vue {
+  @Emit('on-close-explore-sites')
+  public onCloseExploreSites() {}
+}
+</script>
