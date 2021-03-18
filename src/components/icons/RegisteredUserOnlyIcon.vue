f60e374a933be85160d2d9eb2ab865f0bbc8ed35
Zhen Zhang <git@zhen-zhang.com>
2021-03-18 10:43:00 -0700

    Use RegisteredUserOnlyIcon

---
+++ src/components/icons/RegisteredUserOnlyIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-icon v-on="$listeners" v-bind="$attrs">{{ svgPath }}</v-icon>
+</template>
+
+<script>
+import { mdiLock } from '@mdi/js';
+
+export default {
+  data: () => ({
+    svgPath: mdiLock,
+  }),
+};
+</script>
