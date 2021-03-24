cb240405e532d0fdcad055bcbec0188a5d11b860
Zhen Zhang <git@zhen-zhang.com>
2021-03-24 12:11:05 -0700

    Improve form UI

--- src/components/FormResponseCard.vue
+++ src/components/FormResponseCard.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-card>
+  <v-card flat>
     <v-card-title
-      >Response:
-      <a :href="`/forms/${formResponse.form.uuid}`">
+      >表格：
+      <a class="text-decoration-none" :href="`/forms/${formResponse.form.uuid}`">
         {{ formResponse.form.title }}
       </a>
     </v-card-title>
     <div v-if="welcomeTestFormUUID === formResponse.form.uuid" class="ma-2">
-      {{ $t('感谢你花时间回答「新手上路问卷」！') }}
-      <v-btn color="primary" @click="checkWelcomeTestScoreAndClaimRewards">{{
-        $t('查看成绩和领取奖励')
-      }}</v-btn>
+      <div class="ml-2">
+        {{ $t('感谢你花时间回答「新手上路问卷」！') }}
+        <v-btn small depressed color="primary" @click="checkWelcomeTestScoreAndClaimRewards">{{
+          $t('查看成绩和领取奖励')
+        }}</v-btn>
+      </div>
       <v-dialog
         max-width="300"
         v-model="showWelcomeTestResultDialog"
@@ -38,19 +40,23 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer />
-            <v-btn to="/" v-if="claimWelcomeTestScoreMsg.success">{{ $t('回到首页') }}</v-btn>
-            <v-btn @click="retryWelcomeTest" v-else>{{ $t('重试') }}</v-btn>
+            <v-btn small depressed to="/" v-if="claimWelcomeTestScoreMsg.success">{{
+              $t('回到首页')
+            }}</v-btn>
+            <v-btn small depressed @click="retryWelcomeTest" v-else>{{ $t('重试') }}</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
     </div>
     <div>
       <v-card
+        flat
+        outlined
         v-for="field in formResponse.response_fields"
         :key="field.unique_name"
         class="ma-2 pa-4"
       >
-        <h3>{{ field.field_content.desc }}</h3>
+        <span>{{ field.field_type.desc }}</span>
         <div v-if="field.field_content.field_type_name === 'text_response_field'">
           <v-textarea v-model="field.field_content.text" disabled />
         </div>
@@ -58,11 +64,13 @@
           {{ field.field_content.selected_choice }}
         </div>
         <div v-else-if="field.field_content.field_type_name === 'multiple_choices_response_field'">
-          <v-chip-group>
-            <v-chip v-for="choice in field.field_content.selected_choices" :key="choice">{{
-              choice
-            }}</v-chip>
-          </v-chip-group>
+          <v-chip
+            small
+            class="ma-1"
+            v-for="choice in field.field_content.selected_choices"
+            :key="choice"
+            >{{ choice }}</v-chip
+          >
         </div>
       </v-card>
     </div>
@@ -92,7 +100,7 @@ export default class FormResponseCard extends Vue {
   private async mounted() {
     if (this.currentUserUUID === this.formResponse.response_author.uuid) {
       if (env === 'development') {
-        this.welcomeTestFormUUID = '3cyaRakCEkzPW7PRGLmB';
+        this.welcomeTestFormUUID = '48i9bNDXk2NXwp9fELCE';
       } else {
         const data = (await axios.get(prodStateJsonURL)).data;
         this.welcomeTestFormUUID = data['welcome-test-form-uuid'];
