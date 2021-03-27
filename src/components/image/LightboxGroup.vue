75711f5dc66a4e2714c200138f2196e096bf05c0
agdholo <agdholo@gmail.com>
2021-03-27 10:39:26 +0800

    chore(LightBoxGroup): remove style

--- src/components/image/LightboxGroup.vue
+++ src/components/image/LightboxGroup.vue
@@ -33,34 +33,3 @@ export default class LightboxGroup extends Vue {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.lightbox-wrapper {
-  height: 90vh;
-  transform: translate3d(0px, 0px, 0px);
-}
-
-.lightbox-img {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  left: 50%;
-  top: 50%;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-  -webkit-transform: translate3d(-50%, -50%, 0) scaleX(1);
-  transform: translate3d(-50%, -50%, 0) scaleX(1);
-  -webkit-transition: all 0.3s ease;
-  transition: all 0.3s ease;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-
-  img {
-    margin: auto;
-    max-width: 100%;
-    max-height: 100%;
-    vertical-align: center;
-  }
-}
-</style>
