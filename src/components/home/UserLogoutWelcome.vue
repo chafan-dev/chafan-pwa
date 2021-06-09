09046bc2a1ac21187b17904dc54d460af2d3642f
Zhen Zhang <git@zhen-zhang.com>
2021-06-09 00:20:28 -0700

    Fix notifications loading

--- src/components/home/UserLogoutWelcome.vue
+++ src/components/home/UserLogoutWelcome.vue
@@ -1,8 +1,6 @@
 <template>
   <v-sheet>
-    <v-alert outlined type="info">
-      {{ $t('登录后浏览更多个性化内容！') }}
-    </v-alert>
+    <v-alert outlined type="info"> 登录后浏览更多个性化内容！ </v-alert>
 
     <ExploreSitesGrid />
   </v-sheet>
