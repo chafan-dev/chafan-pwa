9eb1d1de3c14680ec4f48836a9979f2151b5152a
Zhen Zhang <git@zhen-zhang.com>
2021-08-01 00:27:28 -0700

    Support reoder exps

---
+++ src/components/icons/DownIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-icon v-bind="$attrs" v-on="$listeners">{{ svgPath }}</v-icon>
+</template>
+
+<script>
+import { mdiChevronDown } from '@mdi/js';
+
+export default {
+  data: () => ({
+    svgPath: mdiChevronDown,
+  }),
+};
+</script>
