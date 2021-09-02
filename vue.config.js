b7380c704feee476e0e3eb9abf660e1c94216f7f
Zhen Zhang <git@zhen-zhang.com>
2021-09-01 20:21:15 -0700

    Fix

--- vue.config.js
+++ vue.config.js
@@ -30,19 +30,7 @@ module.exports = {
         new webpack.ContextReplacementPlugin(
           /highlight\.js\/lib\/languages$/,
           new RegExp(`^./(${['javascript', 'python', 'bash'].join('|')})$`)
-        ),
-        new SentryWebpackPlugin({
-          // sentry-cli configuration - can also be done directly through sentry-cli
-          // see https://docs.sentry.io/product/cli/configuration/ for details
-          authToken: process.env.SENTRY_AUTH_TOKEN,
-          org: 'nomad-research',
-          project: 'chafan-prod',
-          release: process.env.SENTRY_RELEASE,
-
-          // other SentryWebpackPlugin configuration
-          include: './dist',
-          ignore: ['node_modules', 'webpack.config.js'],
-        })
+        )
       );
       if (process.env.SENTRY_AUTH_TOKEN) {
         config.plugins.push(
