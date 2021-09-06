0574efe140e8aec92196ca0f6aac2789ede824a8
Zhen Zhang <git@zhen-zhang.com>
2021-09-05 23:03:41 -0700

    Minor cleanup

--- src/components/home/UserSubmissionsRankedFeed.vue
+++ src/components/home/UserSubmissionsRankedFeed.vue
@@ -11,25 +11,24 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { ISubmission, IUserProfile } from '@/interfaces';
+import { Component } from 'vue-property-decorator';
+import { ISubmission } from '@/interfaces';
 import { dispatchCaptureApiError } from '@/store/main/actions';
 import { apiSubmission } from '@/api/submission';
 import SubmissionPreview from '@/components/SubmissionPreview.vue';
+import { CVue } from '@/common';
 
 @Component({
   components: { SubmissionPreview },
 })
-export default class UserSubmissionsRankedFeed extends Vue {
-  @Prop() public readonly userProfile!: IUserProfile;
-
+export default class UserSubmissionsRankedFeed extends CVue {
   private loadingSubmissions = false;
   private submissions: ISubmission[] | null = null;
 
   private async mounted() {
     await dispatchCaptureApiError(this.$store, async () => {
       this.loadingSubmissions = true;
-      this.submissions = (await apiSubmission.getForUser(this.$store.state.main.token)).data;
+      this.submissions = (await apiSubmission.getForUser(this.token)).data;
       this.loadingSubmissions = false;
     });
   }
