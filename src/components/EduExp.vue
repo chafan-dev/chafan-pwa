accc23160e1671b4a7cbb70666a81123d6db9973
Zhen Zhang <git@zhen-zhang.com>
2021-11-25 10:40:36 -0800

    Refine the EduExp

--- src/components/EduExp.vue
+++ src/components/EduExp.vue
@@ -3,11 +3,12 @@
     <template v-if="compact">
       {{ schoolName }} · {{ levelName }}
       <span v-if="major || enrollYear || graduateYear" class="grey--text">
-        (<span v-if="major">{{ major }}, </span
-        ><span class="grey--text" v-if="enrollYear || graduateYear"
-          ><span v-if="enrollYear">{{ enrollYear }} - </span
-          ><span v-if="graduateYear">{{ graduateYear }}</span
-          ><span v-else>在读</span> </span
+        (<span v-if="major">{{ major }}</span>
+        <span class="grey--text" v-if="enrollYear || graduateYear">
+          <span v-if="major">, </span>
+          <span v-if="enrollYear">{{ enrollYear }} - </span>
+          <span v-if="graduateYear">{{ graduateYear }}</span>
+          <span v-else>在读</span> </span
         >)
       </span>
     </template>
