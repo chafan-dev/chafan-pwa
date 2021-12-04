fd1626d6bbfc889a5555d7c6f7de523bd8b64734
Zhen Zhang <git@zhen-zhang.com>
2021-12-04 12:57:59 -0800

    More validation

---
+++ src/components/base/ValidateUrl.vue
@@ -0,0 +1,6 @@
+<template>
+  <ValidationProvider v-slot="{ errors }" name="url" rules="url">
+    <slot />
+    <span class="error--text">{{ errors[0] }}</span>
+  </ValidationProvider>
+</template>
