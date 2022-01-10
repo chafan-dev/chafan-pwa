25693b9820436fdd3a44b3f44060fcc83a632dff
Zhen Zhang <git@zhen-zhang.com>
2022-01-09 21:32:41 -0800

    Fix #309

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -1,13 +1,140 @@
 <template>
   <div :class="theme.baseCard.classKey" class="pa-3">
-    <div class="row">
-      <div :class="{ 'col-12': !isDesktop }" class="pb-4">
-        <div v-if="avatarURL" :class="{ 'text-center': !isDesktop }" class="pa-3">
+    <template v-if="isDesktop">
+      <div class="row">
+        <div class="col pb-4">
+          <div v-if="avatarURL" class="pa-3 text-center">
+            <router-link :to="`/users/${userPreview.handle}`">
+              <Avatar :avatar-url="avatarURL" size="120" />
+            </router-link>
+          </div>
+          <UserNameHeadline :user-preview="userPreview" class="text-center" />
+          <div v-if="follows" class="text-center">
+            <div>
+              <a
+                v-if="loggedIn && follows.followers_count > 0"
+                class="text-decoration-none"
+                @click="showFollowers"
+              >
+                有{{ follows.followers_count }}个关注者
+                <v-dialog v-model="showFollowersDialog" max-width="800">
+                  <v-card class="pt-6">
+                    <v-card-title
+                      >关注TA的人
+                      <v-spacer />
+                      <CloseIcon @click="showFollowersDialog = false" />
+                    </v-card-title>
+                    <v-card-text>
+                      <DynamicItemList
+                        :load-items="loadFollowers"
+                        v-slot="{ item }"
+                        emptyItemsText="暂无"
+                        nullItemText=""
+                      >
+                        <UserCard :compactMode="true" :userPreview="item" />
+                      </DynamicItemList>
+                    </v-card-text>
+                  </v-card>
+                </v-dialog>
+              </a>
+              <span v-else> 有{{ follows.followers_count }}个关注者 </span>
+            </div>
+
+            <div>
+              <a
+                v-if="loggedIn && follows.followed_count > 0"
+                class="text-decoration-none"
+                @click="showFollowed"
+              >
+                关注了{{ follows.followed_count }}个人
+                <v-dialog v-model="showFollowedDialog" max-width="800">
+                  <v-card class="pt-6">
+                    <v-card-title
+                      >TA关注的人
+                      <v-spacer />
+                      <CloseIcon @click="showFollowedDialog = false" />
+                    </v-card-title>
+                    <v-card-text>
+                      <DynamicItemList
+                        :load-items="loadFollowed"
+                        v-slot="{ item }"
+                        emptyItemsText="暂无"
+                        nullItemText=""
+                      >
+                        <UserCard :compactMode="true" :userPreview="item" />
+                      </DynamicItemList>
+                    </v-card-text>
+                  </v-card>
+                </v-dialog>
+              </a>
+              <span v-else> 关注了{{ follows.followed_count }}个人 </span>
+            </div>
+            <div>
+              <span>Karma：{{ userPreview.karma }}</span>
+            </div>
+            <div class="pt-2" v-if="userPreview.uuid !== currentUserId">
+              <v-btn
+                v-if="follows.followed_by_me"
+                :disabled="cancelFollowIntermediate"
+                class="mt-1 mr-1"
+                depressed
+                small
+                @click="cancelFollow"
+              >
+                取消关注
+                <v-progress-circular v-show="cancelFollowIntermediate" :size="20" indeterminate />
+              </v-btn>
+              <v-btn
+                v-else
+                :disabled="followIntermediate"
+                class="mt-1 mr-1"
+                color="primary"
+                depressed
+                small
+                @click="follow"
+              >
+                关注
+                <v-progress-circular
+                  v-show="followIntermediate"
+                  :size="20"
+                  color="primary"
+                  indeterminate
+                />
+              </v-btn>
+
+              <v-btn
+                :disabled="privateMessageIntermediate"
+                class="mt-1 mr-1"
+                depressed
+                small
+                @click="privateMessage"
+              >
+                私信
+              </v-btn>
+            </div>
+          </div>
+          <v-skeleton-loader type="list-item-three-line" v-else />
+        </div>
+        <div class="col align-self-center">
+          <UserProfileDetails v-if="userPublic" :user-public="userPublic" />
+
+          <div class="d-flex">
+            <v-spacer />
+            <span class="text-caption grey--text mt-2" v-if="userPublic.profile_view_times">
+              这个主页被浏览了{{ userPublic.profile_view_times }}次
+            </span>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <div class="pb-4">
+        <div v-if="avatarURL" class="pa-3 text-center">
           <router-link :to="`/users/${userPreview.handle}`">
             <Avatar :avatar-url="avatarURL" size="140" />
           </router-link>
         </div>
-        <UserNameHeadline v-if="!isDesktop" :user-preview="userPreview" class="text-center" />
+        <UserNameHeadline :user-preview="userPreview" class="text-center" />
         <div v-if="follows" class="text-center">
           <div>
             <a
@@ -114,9 +241,7 @@
         </div>
         <v-skeleton-loader type="list-item-three-line" v-else />
       </div>
-      <div class="col align-self-center">
-        <UserNameHeadline v-if="isDesktop" :user-preview="userPreview" />
-        <v-divider v-if="isDesktop" class="my-2" />
+      <div class="align-self-center">
         <UserProfileDetails v-if="userPublic" :user-public="userPublic" />
 
         <div class="d-flex">
@@ -126,7 +251,7 @@
           </span>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
