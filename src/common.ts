3335bb869076cef8d698f0671c06911799d6a56b
Zhen Zhang <git@zhen-zhang.com>
2022-02-05 13:50:45 -0800

    Fix minor problems

--- src/common.ts
+++ src/common.ts
@@ -8,7 +8,7 @@ import { commitAddNotification } from '@/store/main/mutations';
 import { AxiosError } from 'axios';
 import { isDev, isProdDev } from '@/utils';
 
-export const URLRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?\b([-a-zA-Z0-9()@:,%_\+.~#?&//=]*)/gi;
+export const URLRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?\b([-a-zA-Z0-9()@:,%_\+.~#?&//=]*)$/;
 export const PasswordRegex = /^[\w\-\+@$!%*?&]{8,}$/;
 export const PhoneNumberRegex = /^\+[1-9]\d{1,14}$/;
 
