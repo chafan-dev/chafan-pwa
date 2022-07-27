70e394012d15781e5e35dfbfbb638ef9e1eb173d
Zhen Zhang <git@zhen-zhang.com>
2022-07-26 21:32:25 -0700

    Optimize SiteSearch

--- src/components/SiteSearch.vue
+++ src/components/SiteSearch.vue
@@ -1,33 +1,42 @@
 <template>
-  <v-autocomplete
-    v-model="selectedSiteSubdomain"
-    :item-text="getItemText"
-    :items="sites"
-    :label="label"
-    :loading="loading"
-    :search-input.sync="search"
-    cache-items
-    hide-details
-    hide-no-data
-    placeholder="搜索..."
-    item-value="subdomain"
-    @input="$emit('input', selectedSite)"
-  ></v-autocomplete>
+  <div>
+    <debug-span>{{ selectedSite }}</debug-span>
+    <v-autocomplete
+      v-model="selectedSiteSubdomain"
+      :item-text="getItemText"
+      :items="sites"
+      :label="label"
+      :loading="loading"
+      :search-input.sync="search"
+      cache-items
+      hide-details
+      hide-no-data
+      placeholder="搜索..."
+      item-value="subdomain"
+      @input="$emit('input', selectedSite)"
+    ></v-autocomplete>
+  </div>
 </template>
 
 <script>
 import { apiSearch } from '@/api/search';
 import { readToken } from '@/store/main/getters';
+import DebugSpan from '@/components/base/DebugSpan';
 
 export default {
+  components: { DebugSpan },
   props: {
-    returnSelf: {
-      type: Boolean,
-      default: true,
-    },
     label: {
       type: String,
       default: '圈子',
+    },
+    onStart: {
+      type: Function,
+      default: undefined,
+    },
+    onComplete: {
+      type: Function,
+      default: undefined,
     },
   },
   data() {
@@ -55,10 +64,16 @@ export default {
   },
   methods: {
     querySelections(v) {
+      if (this.onStart) {
+        this.onStart();
+      }
       this.loading = true;
       apiSearch.searchSites(this.token, v).then((response) => {
         this.sites = response.data;
         this.loading = false;
+        if (this.onComplete) {
+          this.onComplete();
+        }
       });
     },
     getItemText(item) {
