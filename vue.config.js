096f818e5dcf247153b32b5e1cd503787363eb53
Zhen Zhang <git@zhen-zhang.com>
2023-03-19 11:35:25 -0700

    Remove _headers

--- vue.config.js
+++ vue.config.js
@@ -75,7 +75,7 @@ module.exports = {
   pwa: {
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
-      exclude: [/\.map$/, /_redirects/],
+      exclude: [/\.map$/, /_redirects/, /_headers/],
       skipWaiting: true,
     },
   },
