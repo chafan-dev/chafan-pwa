de282efccee22f935de30a6c0ad3e82aa8b2f1f9
Chai <bark@bite.com>
2024-11-07 10:26:21 -0500

    Set up local dev env

--- .build_local_dev.sh
+++
@@ -1,8 +0,0 @@
-export VUE_APP_API=dev.cha.fan:4582
-export VUE_APP_NAME=Chafan Dev
-export VUE_APP_DEFAULT_SRC=*
-export VUE_APP_STYLE_SRC=*
-export VUE_APP_FRAME_SRC=*
-export VUE_APP_CONNECT_SRC=*
-yarn run build
-
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    #nixpkgs-23.url = "github:nixos/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils}: {
+    devShell.x86_64-linux =
+        let
+            pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
+            #pkgs_unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+        in pkgs.mkShell {
+            LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+            buildInputs = [
+            pkgs.python312
+            pkgs.yarn
+            pkgs.python312Packages.distutils
+
+
+
+            pkgs.nodePackages.serve
+
+            pkgs.mkcert
+            ];
+        };
+  };
+
+
+
+
+}
